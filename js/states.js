a08cb8af57f28fb3a1e4e085155b9280977af894
Daniel Munoz <daniel.munozdev@gmail.com>
2018-02-14 18:57:33 -0800

    add website v1.0

---
+++ js/states.js
@@ -0,0 +1,61 @@
+// this is for the hamburger menu
+
+var iconHamburger = document.querySelector("#icon-hamburger");
+var showReponsiveNav = document.querySelector(".responsive-nav");
+
+iconHamburger.addEventListener("click", function () {
+    showReponsiveNav.classList.toggle("responsive-nav-show")
+});
+
+
+// listener for all the list items
+
+var menuItems = document.querySelectorAll(".responsive-nav__ul a");
+
+menuItems.forEach(function (item) {
+  item.addEventListener("click", function () {
+      showReponsiveNav.classList.toggle("responsive-nav-show")
+  });
+});
+
+
+// https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          console.log($target)
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
